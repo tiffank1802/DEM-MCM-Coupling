9b8429268a190b21ca02bf9e9bfdb8563dab9cc1
tiffank1802 <tiffank1802@gmail.com>
2026-04-27 12:38:23 +0000

    implementation de la délimitation des partitions calcul de la matrice de transition sur la base de particules de diamètre 8mm

--- DEM_MCM/configs.py
+++ DEM_MCM/configs.py
@@ -2,12 +2,13 @@ from src import run_sweep as r_s
 
 
 configs=[
-    r_s.ExperimentConfig("cartesian",{"nx":5,"ny":3,"nz":5},nlt=10,dt=2,step=50),
-    r_s.ExperimentConfig("cartesian",{"nx":4,"ny":3,"nz":4},nlt=10,dt=2,step=50),
-    r_s.ExperimentConfig("cartesian",{"nx":3,"ny":3,"nz":3},nlt=10,dt=2,step=50),
+    # r_s.ExperimentConfig("cartesian",{"nx":5,"ny":3,"nz":5},nlt=10,dt=2,step=50),
+    # r_s.ExperimentConfig("cartesian",{"nx":4,"ny":3,"nz":4},nlt=10,dt=2,step=50),
+    # r_s.ExperimentConfig("cartesian",{"nx":3,"ny":3,"nz":3},nlt=10,dt=2,step=50),
+    r_s.ExperimentConfig("cylindrical",{"nr":1,"ntheta":10,"nz":1},),
 
 
 ]
 
 
-r_s.run_markov_sweep("cartesian",configs=configs)
+r_s.run_markov_sweep("cylindrical",configs=configs)
--- DEM_MCM/verif_matrix_sum.py
+++ DEM_MCM/verif_matrix_sum.py
@@ -1,5 +1,5 @@
 
 from src.bucket_io import load_experiment_from_bucket
 
-Matrix=load_experiment_from_bucket("physics_400cells_pos_NLT10_step10_dt2_tau50_start250")
+Matrix=load_experiment_from_bucket("cylindrical_nr1_nth10_nz1_equal_area_NLT10_step10_dt2_tau50_start250")
 print(Matrix["matrix"].sum(axis=0))
