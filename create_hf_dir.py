e9fb613f86853413c4f51f34d1ef43fb5717189d
tiffank1802 <tiffank1802@gmail.com>
2026-05-27 18:51:00 +0000

    le fichiers create_hf.py contient le script "simple" pour recuperer les fichiers surr hf notamment la matrice de transition

--- create_hf_dir.py
+++ create_hf_dir.py
@@ -6,17 +6,30 @@ from directory import(
 from huggingface_hub import HfFileSystem
 fs=HfFileSystem()
 
-subdirs=[
-    "ResultsDtMCM",
-"NewResultsMCM",
-"RaffinageTemporel",
-]
 
-    # fs.move(f"{BUCKET_BASE}/{folder}/*",f"{BUCKET_BASE}/{BUCKET_PREFIX}/{folder}")
-# fs.rm(f"{BUCKET_BASE}/{BUCKET_PREFIX}ada*",recursive=True)
-fs.mkdir(f"{BUCKET_BASE}/_Good/Experiment")
-with fs.open(f"{BUCKET_BASE}/_Good/Experiment/.keep" ,"w") as f:
-    f.write("")
+dossiers=fs.ls(f"hf://buckets/{BUCKET_ID}/{BUCKET_PREFIX}")
+path=""
+for dossier in dossiers:
+    if all(j in dossier.get('name', '') for j in ["voronoi_", "dt1", "40cells"]):
+        path=dossier['name']
+import numpy as np
+import matplotlib.pyplot as plt
+P=np.array([])
+with fs.open(f'{path}/transitionmatrix.npy','rb') as f:
+    P=np.load(f)
+    print(P)
+
+fig,ax=plt.subplots()
+fig.set_size_inches(10,10)
+ax.imshow(P)
+for i in range(P.shape[0]):
+    for j in range(P.shape[0]):
+        ax.text(j,i,np.round(P[i,j],3),ha='center',va='center',fontsize=5)
+
+ax.set_title(f'{path.replace("buckets/ktongue/DEM_MCM/Experiments/","")}')
+fig.tight_layout()
+plt.savefig("transition.png")
+
 # with fs.open(f"{BUCKET_BASE}/{BUCKET_PREFIX}/.keep" ,"w") as f:
 #     f.write("")
    
